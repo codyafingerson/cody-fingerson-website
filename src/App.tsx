937be49738310ce0496a7e74aa69e4578369b7c3
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2024-06-06 23:27:10 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-xl text-slate-800 dark:text-white">Coming soon... </h1>
+      <p className="text-md text-slate-800 dark:text-white">
+        Hello there! My updated portfolio is under development, and will be live soon.
+        For now, checkout my GitHub: <a className="hover:underline text-blue-500" href="https://github.com/codyafingerson" target="_blank">@codyafingerson</a>{"\n"}
+         where this project can also be found.
+      </p>
+    </div>
+  )
+}
