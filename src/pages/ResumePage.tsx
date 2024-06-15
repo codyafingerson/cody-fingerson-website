885065007ab3de32739fd225c5e16e19d38fae97
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2024-06-14 21:47:56 -0600

    Create ResumePage

---
+++ src/pages/ResumePage.tsx
@@ -0,0 +1,9 @@
+import Container from "../components/Container"
+
+export default function ResumePage() {
+  return (
+    <Container>
+        <h1 className="text-xl">Welcome to the resume page. It will be updated soon, I promise 😆!</h1>
+    </Container>
+  )
+}
