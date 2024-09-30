2ead616e78f42251e46e7caac72ed9e827d7e2a0
Cody Fingerson <codyfingerson0428@gmail.com>
2024-09-30 00:00:54 -0600

    Add fibonacci utility function

---
+++ src/utils/fibonacci.ts
@@ -0,0 +1,7 @@
+export const fibonacci = (n: number): number[] => {
+    let sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence;
+};
