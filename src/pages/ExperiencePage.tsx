ab504521645c492ed27341df1b92f424d1647607
Cody Fingerson <codyfingerson0428@gmail.com>
2024-09-29 20:34:19 -0600

    Add ExperiencePage component and replace ResumePage component

---
+++ src/pages/ExperiencePage.tsx
@@ -0,0 +1,11 @@
+import Container from "../components/Container"
+
+export default function ExperiencePage() {
+  return (
+    <Container>
+      <h1 className="text-xl">
+        Welcome to the experience page. It will be updated soon, I promise 😆!
+      </h1>
+    </Container>
+  );
+}
--- src/pages/ResumePage.tsx
+++
@@ -1,9 +0,0 @@
-import Container from "../components/Container"
-
-export default function ResumePage() {
-  return (
-    <Container>
-        <h1 className="text-xl">Welcome to the resume page. It will be updated soon, I promise 😆!</h1>
-    </Container>
-  )
-}
