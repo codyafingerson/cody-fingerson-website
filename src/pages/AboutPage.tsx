af34fe1e6843f2b273505f4625ed6edd5bc93315
Cody Fingerson <codyfingerson0428@gmail.com>
2024-09-29 20:34:51 -0600

    Remove AboutPage component

--- src/pages/AboutPage.tsx
+++
@@ -1,9 +0,0 @@
-import Container from "../components/Container";
-
-export default function AboutPage() {
-  return (
-    <Container>
-        <h1 className="text-xl">Welcome to the About page. It will be updated soon, I promise 😆!</h1>
-    </Container>
-  )
-}
