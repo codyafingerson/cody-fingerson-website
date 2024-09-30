e23d60cf8ac1a7c13aa553fb6a78c5e08d7ad28e
Cody Fingerson <codyfingerson0428@gmail.com>
2024-09-29 22:24:33 -0600

    Add ProjectCard component and update ProjectsPage
    - Added ProjectCard component for displaying project information on the ProjectsPage.
    - Updated ProjectsPage to use the ProjectCard component and fetch project data from the projects.ts file.

---
+++ src/data/projects.ts
@@ -0,0 +1,22 @@
+const projects = [
+    {
+        title: "HRDC Homeless Warming Center Software",
+        description:
+            "This custom software solution, developed with a team for our capstone project, enhances the operations of the HRDC’s homeless warming center in Bozeman, Montana. It streamlines guest management for front desk staff, reducing the operational burden and learning curve to ensure more efficient handling of guests.",
+        link: "https://github.com/codyafingerson/hrdc-app",
+    },
+    {
+        title: "State Border Graph",
+        description:
+            "This Java program constructs an undirected graph to represent common borders between U.S. states. It reads data from a file containing state pairs that share borders and maps each state as a vertex. The program outputs a list of states, along with their bordering neighbors, providing a clear visualization of state connectivity.",
+        link: "https://github.com/codyafingerson/StateBorderGraph",
+    },
+    {
+        title: "Insight ERP",
+        description:
+            "Currently in active development, Insight ERP is a full-stack ERP system designed for small to medium-sized businesses. Built using Next.js, MySQL, and TypeScript, it offers a simple and user-friendly interface to manage business operations efficiently.",
+        link: "https://github.com/yourgithub/project-three",
+    },
+];
+
+export default projects;
