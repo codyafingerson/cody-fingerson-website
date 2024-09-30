fb75f5876108c8c8f48178d469a63e142280b7cf
Cody Fingerson <codyfingerson0428@gmail.com>
2024-09-29 20:35:25 -0600

    Refactor Navbar component to update navigation links and add active state

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,38 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom";
 
-export default function Navbar() {  
+export default function Navbar() {
+  const location = useLocation(); // Get the current location
+
+  const isActive = (path: string) =>
+    location.pathname === path
+      ? "text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500"
+      : "text-slate-800 dark:text-white";
+
   return (
     <nav className="bg-white dark:bg-slate-800 p-6">
       <div className="container mx-auto flex items-center justify-between">
-        <Link to="/" className="text-slate-800 dark:text-white hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-yellow-400 hover:via-red-500 hover:to-pink-500">
+        <Link to="/" className="text-slate-800 dark:text-white">
           Cody Fingerson
         </Link>
         <div className="space-x-4">
-          <Link to="/projects" className="text-slate-800 dark:text-white hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-yellow-400 hover:via-red-500 hover:to-pink-500">
+          <Link
+            to="/projects"
+            className={`${isActive(
+              "/projects"
+            )} hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-yellow-400 hover:via-red-500 hover:to-pink-500`}
+          >
             Projects
           </Link>
-          <Link to="/resume" className="text-slate-800 dark:text-white hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-yellow-400 hover:via-red-500 hover:to-pink-500">
-            Resume
-          </Link>
-          <Link to="/about" className="text-slate-800 dark:text-white hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-yellow-400 hover:via-red-500 hover:to-pink-500">
-            About
+          <Link
+            to="/experience"
+            className={`${isActive(
+              "/experience"
+            )} hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-yellow-400 hover:via-red-500 hover:to-pink-500`}
+          >
+            Experience
           </Link>
         </div>
       </div>
     </nav>
-  )
+  );
 }
