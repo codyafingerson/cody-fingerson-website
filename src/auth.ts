7d95dbe7d1d7e6381785ceb73613304755577817
Cody Fingerson <codyfingerson0428@gmail.com>
2025-04-21 19:43:01 -0600

    feat: Implement authentication with NextAuth (with Zoho) and create sign-in page

---
+++ src/auth.ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth"
+import Zoho from "next-auth/providers/zoho"
+import type { Provider } from "next-auth/providers"
+
+const providers: Provider[] = [Zoho]
+
+export const providerMap = providers.map((provider) => {
+    if (typeof provider === "function") {
+        const providerData = provider();
+        return { id: providerData.id, name: providerData.name }
+    } else {
+        return { id: provider.id, name: provider.name }
+    }
+}).filter((provider) => provider.id !== "credentials")
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+    providers,
+    pages: {
+        signIn: "/signin",
+    },
+})
